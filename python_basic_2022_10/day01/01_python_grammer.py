6ad5f39f54d8f78271f9674d2cc25be5004ebb51
sangki930 <sangki930@hanmail.net>
2022-10-19 09:37:47 +0900

    day01_1

---
+++ python_basic_2022_10/day01/01_python_grammer.py
@@ -0,0 +1,27 @@
+
+# 파이썬의 주석
+# ctrl + / 로 주석
+
+print('hello world')
+
+# 프로그램 실행 : 터미널 => python + 파일명
+
+user_age = 36 # int 활용
+
+user_age = 36.5 # float 자료형 변경 가능
+
+print(int(user_age))
+
+# casting => int(), float()
+# 파이썬 자료형 => 숫자형(int, float), 문자열, bool형
+
+num1 = 14
+num2 = 2
+
+print(num1/num2)
+print(num1//num2)
+
+# string (일반문장)도 * 연산 가능
+
+print("test"*4)
+
